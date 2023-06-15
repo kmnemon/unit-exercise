03423ff6f6d99b5b7f10a22a6fc3f120ff22dfc8
ke <ke@kes-MacBook-Pro.local>
2023-06-15 23:41:29 +0800

    d

--- 02_mock_or_stub/src/test/java/tdd/service/GameServiceTest.java
+++ 02_mock_or_stub/src/test/java/tdd/service/GameServiceTest.java
@@ -12,8 +12,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class GameServiceTest {
     private EntityManagerFactory factory;
@@ -33,19 +32,42 @@ public class GameServiceTest {
 
     @Test
     public void getScoreByStateUsingStubAndMockito(){
+        GameService gameService = new GameService(repository);
+        ScoreService scoreService = mock(ScoreService.class);
 
+        when(scoreService.evaluate(mario)).thenReturn(40);
 
+        int score = gameService.getScore(scoreService, "mario").get();
+
+        assertEquals(40, score);
     }
 
     @Test
     public void getScoreByStateUsingStubAndDoNotUsingMockito(){
+        GameService gameService = new GameService(repository);
+        ScoreService scoreService = new FakeScoreService();
 
+        int score = gameService.getScore(scoreService, "mario").get();
 
+        assertEquals(40, score);
+
+    }
+
+    class FakeScoreService extends ScoreService{
+        @Override
+        public int evaluate(Game game){
+            return 40;
+        }
     }
 
     @Test
     public void getScoreByBehaviour(){
+        GameService gameService = new GameService(repository);
+        ScoreService scoreService = mock(ScoreService.class);
 
+        gameService.getScore(scoreService, "mario");
+
+        verify(scoreService).evaluate(mario);
     }
 }
 
